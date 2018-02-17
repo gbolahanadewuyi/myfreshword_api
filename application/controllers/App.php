2a78f7e7af333b92fe748db53b9d0a7d4887443d
peniel armah <peniel.armah@programmer.net>
2018-02-17 17:40:56 +0000

    added api header

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -385,19 +385,26 @@ class App extends REST_Controller {
   // }
 
   public function momo_app_post(){
-      $data = json_decode(file_get_contents('php://input'), TRUE);
-      $query = $this->MyModel->momo_by_id($data['email']);
+      $responseHead = $this->MyModel->header_auth();
+      if($response['status']==200){
+        $data = json_decode(file_get_contents('php://input'), TRUE);
+        $query = $this->MyModel->momo_by_id($data['email']);
 
-      if($query == false){
-        //do nothing
-        $response['success'] = false;
-        $response['message'] = 'User has not set up mobile money';
-      }else{
-        $response['success'] = true;
-        $response['message'] = 'User has set up mobile money';
-        $response['results'] = $query;
+        if($query == false){
+          //do nothing
+          $response['success'] = false;
+          $response['message'] = 'User has not set up mobile money';
+        }else{
+          $response['success'] = true;
+          $response['message'] = 'User has set up mobile money';
+          $response['results'] = $query;
+        }
+        $this->response($response,REST_Controller::HTTP_OK);
       }
-      $this->response($response,REST_Controller::HTTP_OK);
+      else{
+        $this->response($responseHead,REST_Controller::HTTP_NOT_FOUND);
+      }
+
   }
 
   public function credit_card_post(){
