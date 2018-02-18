112219f0c44b932e36a64f7c3e438680f7a5fd15
peniel armah <peniel.armah@programmer.net>
2018-02-18 09:10:54 +0000

    app bug fixing

--- application/models/MyModel.php
+++ application/models/MyModel.php
@@ -504,12 +504,7 @@ class MyModel extends CI_Model {
       $this->db->where('prod_purchase_by',$data['prod_purchase_by']);//by email
       $this->db->where('paid',0);//where product hasnt been paid yet
       $query=$this->db->get();
-      $query->row()->prod_price;
-      if(!isset($query)){
-        return $data = '0';
-      }else {
-        return $query;
-      }
+      return $query->row()->prod_price;
     }
 
     public function fetch_cart_data($data){
