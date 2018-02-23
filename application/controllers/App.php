4bd3b5c9ce35bff18d0e74a22cf495d1f72ffa2f
peniel armah <peniel.armah@programmer.net>
2018-02-23 14:07:49 +0000

    fixing bug

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -501,7 +501,7 @@ class App extends REST_Controller {
     if($response['status']==200){
       $param = json_decode(file_get_contents('php://input'), TRUE);
       //$count = count($param['id']);
-      $this->response($param, REST_Controller::HTTP_OK);
+      $this->response($param .' running from server side', REST_Controller::HTTP_OK);
       // if ($count > 0) {
       //
       //   // for ($i = 0; $i < $count; $i++) {
