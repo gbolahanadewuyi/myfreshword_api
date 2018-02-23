329d203b35dd010646f45de2cd4aef4fa217c7d3
peniel armah <peniel.armah@programmer.net>
2018-02-23 13:50:25 +0000

    app added

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -501,26 +501,28 @@ class App extends REST_Controller {
     if($response['status']==200){
       $param = json_decode(file_get_contents('php://input'), TRUE);
       $count = count($param['id']);
-      if ($count > 0) {
-        for ($i = 0; $i < $count; $i++) {
-          $data = array(
-            'prod_uniqid'     =>  $param['prod_uniqid'],
-            //'prod_description'=>  $param['prod_description'],
-            'prod_name'       =>  $param['prod_name'],
-            'prod_price'      =>  $param['prod_price'],
-            'prod_quantity'   =>  $param['prod_quantity'],
-            'img_link'        =>  $param['prod_img_link'],
-            'user_acc'        =>  $param['prod_purchase_by'],
-            'paid'            =>  1,
-            'file_link'       =>  $param['file_link']
-          );
-          $this->response($data,REST_Controller::HTTP_OK);
-          //$query = $this->MyModel->checkout_data($data);
-          // if($query === true){
-          //   $query = $this->MyModel->delete_cart_data($param);
-          // }
-        }
-      }
+      $this->response($count,REST_Controller::HTTP_OK);
+      // if ($count > 0) {
+      //
+      //   // for ($i = 0; $i < $count; $i++) {
+      //   //   $data = array(
+      //   //     'prod_uniqid'     =>  $param['prod_uniqid'],
+      //   //     //'prod_description'=>  $param['prod_description'],
+      //   //     'prod_name'       =>  $param['prod_name'],
+      //   //     'prod_price'      =>  $param['prod_price'],
+      //   //     'prod_quantity'   =>  $param['prod_quantity'],
+      //   //     'img_link'        =>  $param['prod_img_link'],
+      //   //     'user_acc'        =>  $param['prod_purchase_by'],
+      //   //     'paid'            =>  1,
+      //   //     'file_link'       =>  $param['file_link']
+      //   //   );
+      //   //   $this->response($data,REST_Controller::HTTP_OK);
+      //   //   //$query = $this->MyModel->checkout_data($data);
+      //   //   // if($query === true){
+      //   //   //   $query = $this->MyModel->delete_cart_data($param);
+      //   //   // }
+      //   // }
+      // }
       //an array of data will be passed and stored into the paid database provided if the payment transaction went through
       //once you checkout ==== remove data from cart === then insert into paid products with its associated data
       //we have to count and loop the data object array for insertion
