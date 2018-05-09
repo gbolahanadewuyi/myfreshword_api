6cf1229ce620e3a5ba03a2684b72c21f90511d69
peniel <peniel@techloftgh.com>
2018-05-09 15:44:27 +0000

    fixed syntax bug

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -820,19 +820,19 @@ class App extends REST_Controller {
   //this will be looped twice to the end point
   public function merchant_add_file_post(){
 
-      $config['upload_path']   = base_url().'/uploads/';
+      $config['upload_path']   = 'http://myfreshword.com/myfreshword/api/uploads/';
       $config['allowed_types'] = 'gif|jpg|png';
       $config['max_size']      = 1024;
       $this->load->library('upload', $config);
 
       if ( ! $this->upload->do_upload('image')) {
          $error = array('error' => $this->upload->display_errors());
-         echo json_encode($error);
+         //echo json_encode($error);
          $this->response($error, REST_Controller::HTTP_OK);
       }else {
          $data = $this->upload->data();
          $success = ['success'=>$data['file_name']];
-         echo json_encode($success);
+         //echo json_encode($success);
          $this->response($success, REST_Controller::HTTP_OK);
       }
   }
