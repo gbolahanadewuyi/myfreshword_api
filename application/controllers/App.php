362b5a953970d6dd64506debb45661afef91a73a
peniel <peniel@techloftgh.com>
2018-05-17 18:54:34 +0000

    form validation data success = false

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -955,23 +955,16 @@ class App extends REST_Controller {
   // Here we will go through our form validaitons to avoid same data being posted twice
   public function merchant_add_product_data_post(){
     $_POST = json_decode(file_get_contents('php://input'), TRUE);
-
+    $data= array('success'=> false, 'messages' => array());
     $this->form_validation->set_rules('prod_tags', 'Product Type', 'trim|required');//type
     $this->form_validation->set_rules('prod_name', 'Product Name', 'trim|required|is_unique[ts_products.prod_name]');
     $this->form_validation->set_rules('prod_preacher', 'Product Preacher', 'trim|required');
     $this->form_validation->set_rules('prod_price', 'Product Price', 'trim|required');
     $this->form_validation->set_rules('prod_currency', 'Product Currency', 'trim|required');
-    $this->form_validation->set_rules('prod_description', 'Product Description', 'trim|required');
-    $this->form_validation->set_rules('prod_essay', 'Product Essay', 'trim|required');//
-    $this->form_validation->set_rules('merchant_email', 'Merchant Email', 'trim|required');
-
-
-    $this->form_validation->set_rules('prod_urlname', 'Product Name', 'trim|required');//should be an hidden input
+    $this->form_validation->set_rules('prod_description', 'Product Description', 'trim|required');//this is the theme
+    $this->form_validation->set_rules('prod_essay', 'Product Essay', 'trim|required');//and this is the essay
     $this->form_validation->set_rules('prod_church', 'Church Name', 'trim|required');//should be an hidden input
-    $this->form_validation->set_rules('prod_image', 'Image Name', 'trim|required|is_unique[ts_products.prod_image]');
-    $this->form_validation->set_rules('file_link', 'File Name', 'trim|required');
-
-
+    $this->form_validation->set_rules('merchant_email', 'Merchant Email', 'trim|required');
     $this->form_validation->set_message('is_unique', 'The %s is already taken');
     $this->form_validation->set_error_delimiters('<span class=" text-danger">', '</span>');
     if ($this->form_validation->run() === FALSE){
