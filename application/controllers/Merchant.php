dde540689c9adac49683af0bcce92cc721ad52e1
System Administrator <root@MacBook-Pro.local>
2018-06-06 14:29:06 +0000

    built merchant module and api

---
+++ application/controllers/Merchant.php
@@ -0,0 +1,52 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+require_once APPPATH . '/libraries/REST_Controller.php';
+require_once APPPATH . '/libraries/JWT.php';
+
+use \Firebase\JWT\JWT;
+
+Class Merchant extends REST_Controller{
+
+
+  function __construct(){
+    parent:: __construct();
+    $this->load->model('MyModel');
+    $this->load->model('PayModel', 'pay');
+  }
+
+  function setPaymentDefault_post(){
+    $response = $this->MyModel->merchant_auth();
+    if($response['status']==200){
+      $_POST = json_decode(file_get_contents('php://input'), TRUE);
+        if($_POST['type'] == "momoMerchant"){
+          $query = $this->pay->setMomoDefault($response['id'], $_POST);
+
+
+          return false;
+        }
+
+        if($_POST['type'] == "bankMerchant"){
+          $query = $this->pay->setBankDefault($response['id'], $_POST);
+
+          return false;
+        }
+    }
+    $this->response($response, REST_Controller::HTTP_NOT_FOUND); // BAD_REQUEST (400) being the HTTP response code
+  }
+
+
+  //sp here you should join from both tables
+  function showAdded_payments_get(){
+    $id = (int) $this->get('id');
+    $response = $this->MyModel->merchant_auth();
+    if($response['status']==200){
+      $q['momoRes'] = $this->pay->get_checkMomoPayments($id);
+      $q['bankRes'] = $this->pay->get_checkBankPayments($id);
+      $this->response($q,REST_Controller::HTTP_OK);
+    }
+    $this->response($response, REST_Controller::HTTP_NOT_FOUND); // BAD_REQUEST (400) being the HTTP response code
+  }
+
+
+
+}//end of class
---
+++ application/models/PayModel.php
@@ -0,0 +1,43 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+Class PayModel extends CI_Model {
+
+  protected $momoTable = "merchant_momo";
+  protected $bankTable = "merchant_bank";
+
+  function __construct(){
+    parent:: __construct();
+  }
+
+
+  function get_checkMomoPayments($merchant_id){
+    return $query = $this->db->select()->from($this->momoTable)->where('merchant_id', $merchant_id)->get()->row();
+  }
+
+  function get_checkBankPayments($merchant_id){
+    return $query = $this->db->select()->from($this->bankTable)->where('merchant_id', $merchant_id)->get()->row();
+  }
+
+
+  function setMomoDefault($merchant_id, $data){
+    $data = array('defaultSet'=>1);
+    $query =$this->db->where('merchant_id',$merchant_id)->update($this->momoTable,$data);
+    if($query == true){
+      return array('status'=>201, 'message'=>'Mobile Money set as default payment channel');
+    }
+    return array('status'=>400, 'message'=> 'Error setting mobile money as default payment channel');
+  }
+
+
+  function setBankDefault($merchant_id, $data){
+    $data = array('defaultSet'=>1);
+    $query =$this->db->where('merchant_id',$merchant_id)->update($this->bankTable,$data);
+    if($query == true){
+      return array('status'=>201, 'message'=>'Bank Account set as default payment channel');
+    }
+    return array('status'=>400, 'message'=> 'Error setting bank account as default payment channel');
+  }
+
+
+}
