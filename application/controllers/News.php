b7e5c42752cb8230b887814451d639a466b298c5
System Administrator <root@MacBook-Pro.local>
2018-06-14 11:21:47 +0000

    feedpage get module

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -10,6 +10,7 @@ Class News extends REST_Controller{
   function __construct(){
     parent:: __construct();
     $this->load->model('NewsFeedModel', 'news');
+    $this->load->model('MyModel', 'my');
   }
 
   function index_get(){
@@ -20,5 +21,21 @@ Class News extends REST_Controller{
     }
     $this->response($q, REST_Controller::HTTP_OK);
   }
-  
+
+
+  //get user feed from this endpoint 
+  function feed_get(){
+    $r = $this->my->header_auth();
+    if($r['status']==200){
+      $q = $this->news->get_all_feed_data();
+      if($q['status'] == 204 ){
+        $this->response($q, REST_Controller::HTTP_NO_CONTENT);
+        return false;
+      }
+      $this->response($q, REST_Controller::HTTP_OK);
+      return false;
+    }
+    $this->response($r,REST_Controller::HTTP_NOT_FOUND);
+  }
+
 }
--- application/models/NewsFeedModel.php
+++ application/models/NewsFeedModel.php
@@ -4,6 +4,7 @@
 Class NewsFeedModel extends CI_Model {
 
   protected $newsTable = "news_feed_category";
+  protected $feedTable = "merchant_feed";
   public function __construct(){
     parent:: __construct();
 
@@ -11,6 +12,14 @@ Class NewsFeedModel extends CI_Model {
 
   function get_topic_category(){
     $q = $this->db->select('*')->from($this->newsTable)->get()->result();
+    if($q == true){
+      return array('status'=>200, 'result'=>$q);
+    }
+    return array('status'=>204, 'message'=> 'No Content found');
+  }
+
+  function get_all_feed_data(){
+    $q  = $this->db->select('id, title, message, image, timestamp, likes_count, comments_count')->from($this->newsTable)->get()->result();
     if($q == true){
       return array('status'=>200, 'result'=>$q);
     }
