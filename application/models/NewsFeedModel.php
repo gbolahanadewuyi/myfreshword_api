482eb3eb76af006db599f852f908e5b8c5722896
peniel <peniel@techloftgh.com>
2018-06-20 11:35:27 +0000

    Changed news feed response for nathan

--- application/models/NewsFeedModel.php
+++ application/models/NewsFeedModel.php
@@ -5,7 +5,7 @@ Class NewsFeedModel extends CI_Model {
 
   protected $newsTable = "news_feed_category";
   protected $feedTable = "merchant_feed";
-  
+
   public function __construct(){
     parent:: __construct();
 
@@ -22,7 +22,8 @@ Class NewsFeedModel extends CI_Model {
   function get_all_feed_data(){
     $q  = $this->db->select('id, title, message, image, timestamp, likes_count, comments_counts')->from($this->feedTable)->get()->result();
     if($q == true){
-      return array('status'=>200, 'result'=>$q);
+      //return array('status'=>200, 'result'=>$q);
+      return $q;
     }
     return array('status'=>204, 'message'=> 'No Content found');
   }
