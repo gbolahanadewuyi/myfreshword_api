c38633cd5ebab0c7ae656dfb6689f328a4d4fcf2
peniel <peniel@techloftgh.com>
2018-06-25 09:18:43 +0000

    fixing bug

--- application/controllers/Notifications.php
+++ application/controllers/Notifications.php
@@ -20,10 +20,9 @@ Class Notifications extends REST_Controller{
     function index_get(){
       $response = $this->MyModel->merchant_auth();
       if($response['status']==200){
-        $id = $this->get('id');//serves as the primary key
-        $id = (int) $id;
+        $id = (int) $this->get('id');
 
-        if($id == ""){
+        if($id === NULL){
           $q = $this->note->get_all_notifications($response['id']);
           if($q['status'] == 200){
             $this->response($q, REST_Controller::HTTP_OK);
@@ -32,13 +31,16 @@ Class Notifications extends REST_Controller{
           $this->reponse($q, REST_Controller::HTTP_NOT_FOUND);
           return false;
         }
-        $b = $this->note->get_notification_id($response['id'], $id);
-        if($b['status'] == 200){
-          $this->response($b, REST_Controller::HTTP_OK);
+        else{
+          $b = $this->note->get_notification_id($response['id'], $id);
+          if($b['status'] == 200){
+            $this->response($b, REST_Controller::HTTP_OK);
+            return false;
+          }
+          $this->response($b, REST_Controller::HTTP_NOT_FOUND);
           return false;
         }
-        $this->response($b, REST_Controller::HTTP_NOT_FOUND);
-        return false;
+
       }
       $this->response($response, REST_Controller::HTTP_NOT_FOUND); // BAD_REQUEST (400) being the HTTP response code
     }
