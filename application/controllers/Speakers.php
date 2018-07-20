138a996a155f43f9622b456f35d9d5d9151373df
peniel <peniel@programmer.net>
2018-07-20 19:27:22 +0000

    speaker by id modified

--- application/controllers/Speakers.php
+++ application/controllers/Speakers.php
@@ -6,9 +6,12 @@ require_once APPPATH . '/libraries/JWT.php';
 use \Firebase\JWT\JWT;
 
 Class Speakers extends REST_Controller{
+
   function __construct(){
     parent:: __construct();
     $this->load->model('SpeakerModel', 'sp');
+    $this->load->model('MyModel');
+
   }
 
   function index_get(){
@@ -19,4 +22,32 @@ Class Speakers extends REST_Controller{
     }
     $this->response($q, REST_Controller::HTTP_OK);
   }
+
+  function search_speaker_post(){
+    $response = $this->MyModel->header_auth();
+    if($response['status']==200){
+      $_POST = json_decode(file_get_contents('php://input'), TRUE);
+      $q = $this->MyModel->search_speaker($_POST['feed_search']);
+      if(count($q) > 0){
+        $this->response($q, REST_Controller::HTTP_OK);
+      }
+      else {
+        $this->response($q, REST_Controller::HTTP_NO_CONTENT);
+      }
+    }
+    else{
+      $this->response($response,REST_Controller::HTTP_NOT_FOUND);
+    }
+  }
+
+ function author_id_get(){
+   $id = (int) $this->get('id');
+   $q = $this->sp->get_speaher_id($id);
+   if($q == ""){
+     $this->response($q, REST_Controller::HTTP_NO_CONTENT););
+     return false;
+   }
+   $this->response($q, REST_Controller::HTTP_OK);
+ }
+
 }
--- application/models/SpeakerModel.php
+++ application/models/SpeakerModel.php
@@ -2,7 +2,9 @@
 
 Class SpeakerModel extends CI_Model {
 
-  protected $speakerTable = "speakers";
+  protected $speakerTable     = "speakers";
+  protected $speakerFollowers = "speakers_followers";
+
 
   function __construct(){
     parent:: __construct();
@@ -16,7 +18,57 @@ Class SpeakerModel extends CI_Model {
     return array('status'=>204, 'message'=> 'No Content found');
   }
 
+
+  function get_speaher_id($id){
+    return $this->db->select('*')->from($this->speakerTable)->where('id',$id)->order_by('id','desc')->get()->row();
+  }
+
+
+
   function add_to_list($data){
     $q =$this->db->insert($this->speakerTable, $data);
+  }
+
+
+  function search_speaker($search_term){
+    $search_term="%".$search_term."%";
+    $sql="SELECT * FROM $this->speakerTable WHERE name LIKE ? ";
+    $query=$this->db->query($sql,array($search_term));
+    $res=$query->result();//so basically we are going to return an array of the results
+     if(count($res) > 0){
+       return $res;
+     }
+     else {
+       return array('status'=>400 , 'message'=> 'Sorry No results found');
+     }
+  }
+
+  function new_follow_speaker($data){
+    $query = $this->db->insert($this->speakerFollowers, $data);
+    return $query;
+  }
+
+
+  function adding_followers(){
+
+  }
+
+  //so if there isnt any followers or if there is
+  function check_followers($user_id, $data){//user id will be passed by the token
+    $q  = $this->db->select('ts_users_id')->from($this->speakerFollowers)->where('ts_users_id',$user_id)->get()->row();
+    if($q == ""){
+      //so here you have to insert data into the database
+       return $q = $this->new_follow_speaker($data);
+    }
+    return array('status'=>200, 'message'=> 'User is already following speaker');
+  }
+
+
+  //if a user decides to unfollow the speaker
+  function unfollow_speaker($user_id){
+    $q = $this->db->where('ts_users_id',$user_id)->delete($this->speakerFollowers);
+    if($q == true){
+      return array('status'=>204, 'message'=> 'Speaker unfollowed successfully');
+    }
   }
 }
