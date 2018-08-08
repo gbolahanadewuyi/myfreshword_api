fb2860fe1f26a3fadcb08f33284fe699e32405f4
kuuku.aboagye <kuuku@techloftgh.com>
2018-08-08 13:55:52 +0000

    followers2

--- application/controllers/Speakers.php
+++ application/controllers/Speakers.php
@@ -62,7 +62,7 @@ Class Speakers extends REST_Controller{
        return false;
      }
      $resp = $q;
-     $resp['followers']  = $j;
+     $resp['followers']  = $j['followers'];
     $this->response($resp, REST_Controller::HTTP_OK);
    }
    else{
