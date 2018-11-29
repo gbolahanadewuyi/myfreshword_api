3de13da2fae980855c70a74f096c814299139ea1
BabaTunde <babatunde@techloftgh.com>
2018-11-29 16:36:38 +0000

    minor changes

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -941,8 +941,8 @@ class App extends REST_Controller
 	// this shooud be the response for the payment
 
 	public function upload_profile_picture_post(){
-		 $filename = $this->get('photo');   
-		 echo $filename;  
+		 $filename = (string)$this->get('photo');   
+		 return $filename;  
 		require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
 		// use google\appengine\api\cloud_storage\CloudStorageTools;
 
