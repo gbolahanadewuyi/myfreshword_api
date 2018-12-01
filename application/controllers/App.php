74bde2b7e4d6aa41593d34cb3806a36a7d355b82
BabaTunde <babatunde@techloftgh.com>
2018-12-01 12:52:14 +0000

    upload path issue fix

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -953,7 +953,8 @@ class App extends REST_Controller
 			$config['max_width']     = 1024; 
 			$config['max_height']    = 768;  
 			$this->load->library('upload', $config);
-			   
+			
+			$this->upload->initialize($config);
 			if ( ! $this->upload->do_upload('userfile')) {
 			//    $error = array('error' => $this->upload->display_errors()); 
 			// //    $this->load->view('upload_form', $error); 
