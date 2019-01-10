467a2a5da19736e807267430c574f7f9115ed7ba
BabaTunde <babatunde@techloftgh.com>
2019-01-10 14:12:45 +0000

    adjusting get_speaker data to include pastor id

--- application/models/SpeakerModel.php
+++ application/models/SpeakerModel.php
@@ -20,7 +20,7 @@ Class SpeakerModel extends CI_Model {
 
 function get_speaker_data($query){
       // $q = $this->db->select('*')->from($this->speakerTable)->get()->result_array();//getting object array returns id of the speaker
-      $q = $this->db->select('pastors_lisiting.id,pastors_listing.pastors_title, pastors_listing.pastors_name, pastors_listing.pastors_bio, pastors_listing.pastors_avatar_img, ts_merchant.organisation')->from('pastors_listing')->join('ts_merchant','pastors_listing.merchant_id = ts_merchant.id','Left')->get()->result_array();
+      $q = $this->db->select('pastors_listing.id,pastors_listing.pastors_title, pastors_listing.pastors_name, pastors_listing.pastors_bio, pastors_listing.pastors_avatar_img, ts_merchant.organisation')->from('pastors_listing')->join('ts_merchant','pastors_listing.merchant_id = ts_merchant.id','Left')->get()->result_array();
       $que = $this->db->select('*')->from($this->speakerFollowers)->where('ts_users_id', $query)->get()->result_array();//returns speaker_id
 
 
