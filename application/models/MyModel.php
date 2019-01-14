21340cf9f3b08adab032a066415b8aad517feb0f
BabaTunde <babatunde@techloftgh.com>
2019-01-14 13:08:43 +0000

    minor changes

--- application/models/MyModel.php
+++ application/models/MyModel.php
@@ -2384,14 +2384,14 @@ class MyModel extends CI_Model
 	{
 		$this->db->select_sum('likes_count')->from('merchant_feed')->where('churchid', $id);
 		$q = $this->db->get();
-		return $q;
+		return $q->likes_count;
 	}
 
 	public function total_comments($id)
 	{
 		$this->db->select_sum('comments_counts')->from('merchant_feed')->where('churchid', $id);
 		$q = $this->db->get();
-		return $q;
+		return $q->comments_counts;
 	}
 
 
