c5471a9f9ff5be09b70535ecfd3e1b6ab69677a3
BabaTunde <babatunde@techloftgh.com>
2019-01-18 16:52:11 +0000

    adjusted the query at the search_speaker model

--- application/models/SpeakerModel.php
+++ application/models/SpeakerModel.php
@@ -138,7 +138,7 @@ function get_speaker_data($query){
      $sql = "SELECT concat(pastors_listing.pastors_title,' ',pastors_listing.name) as name, pastors_listing.id, pastors_listing.bio, pastors_listing.photo, ts_merchant.organisation
      from pastors_listing 
      left join ts_merchant on pastors_listing.merchant_id = ts_merchant.id
-     where name like '?'";
+     where name like '_'";
 
      
 
