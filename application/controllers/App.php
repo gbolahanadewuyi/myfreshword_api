0ada5aa4080d3822eb43ea4a054be4c2cd482568
BabaTunde <babatunde@techloftgh.com>
2019-01-23 11:28:07 +0000

    endpoint for profile update

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -944,8 +944,7 @@ class App extends REST_Controller
 				'success' => false,
 				'messages' => array()
 			);
-			$this->form_validation->set_rules('username', 'Username', 'trim');
-			$this->form_validation->set_rules('email', 'Email', 'trim');
+			$this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[5]|is_unique[ts_user.user_uname]');
 			$this->form_validation->set_rules('mobile', 'Mobile', 'trim');
 			$this->form_validation->set_error_delimiters('<span class=" text-danger">', '</span>');
 			if ($this->form_validation->run() === false) {
